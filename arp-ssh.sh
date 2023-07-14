2b88a638ef0e09c260162fcd1c18833e5fac2a90
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2023-07-14 17:14:27 +0200

    arp-ssh: fix sudo

--- arp-ssh.sh
+++ arp-ssh.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+# run as root
 
 interface=$1
 
 # Scan the local network interface for devices
-devices=$(sudo arp-scan --interface=$interface --localnet | fzf)
+devices="`sudo arp-scan --interface=$interface --localnet | sed '1,2d'`"
+selected="`(echo "$devices" | fzf)`"
 
-# If no devices were found, exit
-if [[ -z $devices ]]; then
-  echo "No devices found."
+
+if [[ -z "$selected" ]]; then
   exit 1
 fi
 
 # Get the IP address of the selected device
-ip=$(sudo arp-scan --interface=$interface --localnet | grep "$devices" | awk '{print $1}')
+ip=$(sudo arp-scan --interface=$interface --localnet | grep "$selected" | awk '{print $1}')
 
 # Connect to the device using SSH
 ssh root@$ip
