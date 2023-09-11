f14bbe3aaec74b537f71b4ab47f959660d50fdef
Timon van der Berg <timon.vanderberg@streamunlimited.com>
2023-09-11 12:40:02 +0200

    better ergo, configurable user, auto-detect interface

--- arp-ssh.sh
+++ arp-ssh.sh
@@ -3,8 +3,18 @@
 interface=$1
 
 if [[ -z "$interface" ]]; then
-  echo "Usage: arp-ssh enp10s0"
-  exit 1
+  # Use the ifconfig command to get network interface information
+  ifconfig_output=$(ifconfig)
+
+  # Use grep and awk to extract the name of the active interface
+  interface=$(ip -o link show | awk '$2 != "lo:" && $9 == "UP" {print $2}' | cut -d ":" -f 1)
+
+  if [[ -z "$interface" ]]; then
+    echo "Error: failed to auto-detect active network interface."
+    exit 1
+  fi
+
+  echo "Active network interface: $interface"
 fi
 
 # Scan the local network interface for devices
@@ -19,8 +29,15 @@ fi
 # Get the IP address of the selected device
 ip=$(sudo arp-scan --interface=$interface --localnet | grep "$selected" | awk '{print $1}')
 
-echo "connecting to $selected"
+echo "connecting to $selected" 
+echo -n "user (ENTER for root):"
+
+read user_input
+
+if [[ -z $user_input ]]; then
+  user_input="root"
+fi
 
 # Connect to the device using SSH
-ssh-retry $ip
+ssh-retry "$user_input@$ip"
 
